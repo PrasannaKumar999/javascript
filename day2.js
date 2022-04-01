7a5bcdb9c8f2acb231569cf8cb3f0e14c4ab102c
PrasannaKumar999 <97685872+PrasannaKumar999@users.noreply.github.com>
2022-04-01 19:37:36 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,63 @@
+//for loops
+//let i=0;
+
+//for(;i<10;){
+//console.log("value of i is",i);
+//  i++;
+//}
+
+//while loops
+//let i=0;
+//while (i<0) {
+//    console.log("value of i is",i);
+//    i++;
+//}
+//console.log("outside loop");
+
+//do-while loop
+
+//let i=0;
+
+//do{
+//    console.log("value of i is",i);
+//    i++;
+//}while(i<10)
+
+//nested loop
+
+//for(let i=0;i<5;i++){
+//    for(let j=0;j<2;j++){
+//        console.log("value of i is",i,"and j is",j)
+//   }
+//}
+
+
+// let a=20;
+// let b=30;
+// function addtwonumbers(a,b){
+//     console.log(a+b);
+// }
+// addtwonumbers(22,23)
+
+// // function
+// //  let a=10;
+// //  let b=12;
+// //  function add (){ // function definition
+// //     console.log(a+b);
+// //  }
+// //  add(); // function execution
+//  // function with parseFloatmetr
+// //   function addtwo(a,b){
+// //       console.log(a+b);
+// //   }
+// //   addtwo(15000,20);
+//   // arguments : the value which pass to a variable (15000 and 20)
+//   // parameter : the value  which function use to do the opration (a and b)
+ 
+//   // 3 function with return statement
+//   function set(x,y){
+//     return x+y; // any code written after return is not accessible
+// }
+// let res=set(12,13); // coz it  return something so we have to put that result in some variable 
+// console.log(res);
+a
